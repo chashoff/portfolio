0026403b378169188ba7b1d3c88a131e11b31c22
chashoff <chas.hoff@protonmail.com>
2020-09-12 22:59:22 -0400

    Added try catch for axios form call

--- reactJs/src/views/Contact.js
+++ reactJs/src/views/Contact.js
@@ -3,6 +3,7 @@ import '../styles/Contact.css';
 import axios from '../../node_modules/axios';
 
 function Contact(){
+    const [btnText, setBtn] = useState('Submit');
     const [name, setName] = useState('');
     const [email, setEmail] = useState('');
     const [reason, setReason] = useState('');
@@ -17,14 +18,19 @@ function Contact(){
             reason,
             message
         }
-
-        axios.post("http://localhost:4000/api/sendForm", dataToSubmit);
+        axios.post("http://localhost:4000/api/sendForm", {
+            dataToSubmit
+        })
+        .then((res) => {
+            if(res.status === 200) {
+                setBtn('Form Submitted!');
+            }
+        }, (error) => {
+            console.log(error);
+            setBtn('Submission Failed...');
+        });
     }
-
     
-
-
-    console.log(name)
     return(
         <div className="contact-container"> 
             <div style={{position: 'relative'}} >
@@ -37,7 +43,7 @@ function Contact(){
                     <input className="contact-input" type="email" id="email" name="email" placeholder="Email" value={email} onChange={e => setEmail(e.target.value)} />
                     <input className="contact-input" type="text" id="reason" name="reason" placeholder="Reason..." value={reason} onChange={e => setReason(e.target.value)} />
                     <textarea rows="6" className="contact-input" type="message" id="message" name="message" placeholder="Your message..." value={message} onChange={e => setMessage(e.target.value)} />
-                    <button className="contact-submit-btn" type="submit">Submit</button>
+                    <button className="contact-submit-btn" type="submit">{btnText}</button>
                 </form>
             </div>
         </div>
